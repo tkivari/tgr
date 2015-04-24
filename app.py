cfa76dc398165117a1ed1a6e00b07d12ceaad988
Tyler Kivari <tkivari@adknowledge.com>
2015-04-24 11:56:26 -0400

    fix stuff

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, request
-from TwitterAPI import TwitterAPI
 import pymongo
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+  return "Hello World!"
 
-
-@app.route("/")
 if __name__ == "__main__":
-    app.run()
+  app.run()
