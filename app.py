e2f2224059458aa72e1b3e051972471354774895
sunshine <ty.kivari@gmail.com>
2015-04-27 09:13:49 -0400

    got blueprints to work

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from tags import tag_api
-from twitter import twitter_api
+from controllers.tags import tag_api
+from controllers.twitter import twitter_api
 app = Flask(__name__)
 
 app.config.from_pyfile('config.py', silent=True)
 
 app.register_blueprint(tag_api, url_prefix='/tags')
-#app.register_blueprint(twitter_api, url_prefix='/twitter')
+app.register_blueprint(twitter_api, url_prefix='/twitter')
 
 @app.route('/')
 def index():
